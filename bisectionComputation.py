84481007e84ce0fb6500964f8cd8bd11985d4fb1
SlygoPika <hmorindg25@gmail.com>
2023-02-01 11:23:18 -0500

    Added dumb-proofing to bisection method

--- bisectionComputation.py
+++ bisectionComputation.py
@@ -3,7 +3,10 @@ from math import log10
 def compute(a, b, tol, equation):
     error = (b - a) / 2
 
-    while error >= tol:
+    if (equation(a) > 0 and equation(b) > 0) or (equation(a) < 0 and equation(b) < 0):
+        return -1
+    
+    while error > tol:
         midPoint = (a + b) / 2
 
         increasing = equation(a) < equation(b)
